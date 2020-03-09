eb93f0e3e83b0cd40f1fa7326b27920d8f7ce7f8
Terthann <60939529+Terthann@users.noreply.github.com>
2020-03-08 17:33:57 -0700

    Outlined Menu Requirements

--- JDBC Project/src/Menu/Menu.java
+++ JDBC Project/src/Menu/Menu.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Menu;
 
-/**
- *
- * @author Andrew
- */
-public class Menu {
-    
+public class Menu
+{
+    public void start()
+    {
+        System.out.println("Welcome!");
+        
+        do
+        {
+            
+        } while (true);
+    }
+    // List all writing groups.
+    // List all data for a group specified by the user.
+        // Includes all data for the associated books and publishers.
+    // List all publishers.
+    // List all data for a publisher specified by the user.
+        // Includes all data for the associated books and writing groups.
+    // List all book titles.
+    // List all data for a single book specified by the user.
+        // Includes all data for the associated publisher and writing group.
+    // Insert new book.
+    // Insert new publisher and update books from a previous publisher to the new one.
+        // Add publisher first.
+        // Then update books.
+    // Remove single book specified by the user.
 }
