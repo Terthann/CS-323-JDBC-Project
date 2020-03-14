054ea9d3b7dc6e4068a174ccc7cc16110bcbcdcf
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2020-03-14 01:24:25 -0700

    Update PublisherQuery.java
    Added insertBook() method for choice #7, not used by PublisherQuery

--- JDBC Project/src/Query/PublisherQuery.java
+++ JDBC Project/src/Query/PublisherQuery.java
@@ -27,4 +27,16 @@ public class PublisherQuery extends Query
         System.out.println("Phone #: " + phone);
         System.out.println("E-mail: " + email + "\n");
     }
+    
+    // Inserts new Book into the table.
+    // NOTE: Not used by PublisherQuery or WritingGroupQuery
+    // received "error: cannot find symbol" since userQuery isn't explicity declared as a BookQuery
+    // fixed by implementing method into abstract class and its subclasses.
+    @Override
+    public String insertBook()
+    {
+        // SQL Query
+        return "Insert Into books Values " +
+                "(?, ?, ?, ?, ?)";
+    }
 }
